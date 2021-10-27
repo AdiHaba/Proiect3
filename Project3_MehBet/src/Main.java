4463b1e5f5c0195e3c2f0c3d3e0ba25224fcd79d
AdiHaba <adrianhaba@outlook.com>
2021-10-27 11:38:10 +0300

    Am bagat hitzu in schemaa

--- Project3_MehBet/src/Main.java
+++ Project3_MehBet/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("gg");
         System.out.println("ff");
-        System.out.println("hitz");
+        System.out.println("hitzz");
     }
 }
