2319dea511e4eae4e9fdfab55513cc598643c023
iosifdaniel07 <iosifdaniel07@yahoo.com>
2021-10-27 14:13:14 +0300

    SlotMachine tentativa

---
+++ Project3_MehBet/src/app/gui/SlotMachine.java
@@ -0,0 +1,50 @@
+package app.gui;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class SlotMachine {
+
+    private JPanel _mainPanel, _centerPanel;
+    private JFrame _frame;
+    private BufferedImage bg = null;
+
+
+    public SlotMachine(){
+
+        _mainPanel = new JPanel();
+        _mainPanel.setLayout(new BorderLayout());
+
+        myCenterPanel();
+        _mainPanel.add(_centerPanel,BorderLayout.CENTER);
+
+        myFrame();
+        _frame.setVisible(true);
+    }
+
+    public void myCenterPanel(){
+
+        _centerPanel = new JPanel();
+        _centerPanel.setLayout(new GridLayout(3,5));
+
+        try{
+            bg = ImageIO.read(getClass().getResource("seven.jpg"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void myFrame(){
+        _frame = new JFrame("Slot");
+        _frame.setContentPane(_mainPanel);
+        _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
