3d0bfb941d63cf0e673d1323c1da7664e4a6c381
iosifdaniel07 <iosifdaniel07@yahoo.com>
2021-10-27 14:42:06 +0300

    SlotMachine tentativa2

--- Project3_MehBet/src/app/gui/SlotMachine.java
+++ Project3_MehBet/src/app/gui/SlotMachine.java
@@ -1,10 +1,8 @@
 package app.gui;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class SlotMachine {
 
@@ -18,33 +16,47 @@ public class SlotMachine {
         _mainPanel = new JPanel();
         _mainPanel.setLayout(new BorderLayout());
 
-        myCenterPanel();
-        _mainPanel.add(_centerPanel,BorderLayout.CENTER);
+       _centerPanel = new JPanel();
+       _centerPanel.setLayout(new GridLayout(3,5));
+       myCenterPanel();
+
+       _mainPanel.add(_centerPanel,BorderLayout.CENTER);
 
         myFrame();
         _frame.setVisible(true);
     }
 
     public void myCenterPanel(){
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
 
-        _centerPanel = new JPanel();
-        _centerPanel.setLayout(new GridLayout(3,5));
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
 
-        try{
-            bg = ImageIO.read(getClass().getResource("seven.jpg"));
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotMove());
+        _centerPanel.add(new SlotStable());
     }
 
     public void myFrame(){
         _frame = new JFrame("Slot");
         _frame.setContentPane(_mainPanel);
         _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        _frame.setLocation(500,90);
+        _frame.setSize(700,400);
     }
 
     public static void main(String[] args) {
-
+      SlotMachine s = new SlotMachine();
     }
+
+
 }
---
+++ Project3_MehBet/src/app/gui/SlotStable.java
@@ -0,0 +1,41 @@
+package app.gui;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class SlotStable extends JPanel {
+
+    private BufferedImage bg = null;
+    private int yOffset = 0;
+    private int yDelta = 4;
+
+    public SlotStable(){
+
+        try{
+            bg = ImageIO.read(getClass().getResource("lamaie.jpg"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+       return new Dimension(115,155);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g.create();
+
+          g2d.drawImage(bg,0,0,this);
+
+    }
+
+}
