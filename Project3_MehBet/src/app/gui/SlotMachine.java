3641b3b3cac9ff1fa3ce9676791c079eb85266ad
iosifdaniel07 <iosifdaniel07@yahoo.com>
2021-10-28 23:07:56 +0300

    SlotMachine 35%

--- Project3_MehBet/src/app/gui/SlotMachine.java
+++ Project3_MehBet/src/app/gui/SlotMachine.java
@@ -3,24 +3,20 @@ package app.gui;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Random;
 
 public class SlotMachine {
 
     private JPanel _mainPanel, _centerPanel;
     private JFrame _frame;
+    private JButton _spin;
+    private SlotStable[][] _slots;
+    private int[][] _valueSlots;
     private BufferedImage _cireasa = null , _lamaie = null, _lebenita = null, _portocale = null,
                           _pruna = null, _struguri = null, _sapte = null;
-    private SlotStable _linia1Col1, _linia1Col2, _linia1Col3, _linia1Col4, _linia1Col5;
-    private SlotStable _linia2Col1, _linia2Col2, _linia2Col3, _linia2Col4, _linia2Col5;
-    private SlotStable _linia3Col1, _linia3Col2, _linia3Col3, _linia3Col4, _linia3Col5;
-    private JButton _spin;
-    private Integer _nrLinia1Col1, _nrLinia1Col2,_nrLinia1Col3, _nrLinia1Col4, _nrLinia1Col5;
-    private Integer _nrLinia2Col1,  _nrLinia2Col2,  _nrLinia2Col3, _nrLinia2Col4, _nrLinia2Col5;
-    private Integer _nrLinia3Col1, _nrLinia3Col2, _nrLinia3Col3, _nrLinia3Col4, _nrLinia3Col5;
+
 
     public SlotMachine(){
 
@@ -33,11 +29,7 @@ public class SlotMachine {
 
        _mainPanel.add(_centerPanel,BorderLayout.CENTER);
        _spin = new JButton("Spin");
-       _spin.addActionListener(new SpinEvent(_nrLinia1Col1, _nrLinia1Col2,_nrLinia1Col3, _nrLinia1Col4, _nrLinia1Col5,
-               _nrLinia2Col1,  _nrLinia2Col2,  _nrLinia2Col3, _nrLinia2Col4, _nrLinia2Col5,_nrLinia3Col1, _nrLinia3Col2,
-               _nrLinia3Col3, _nrLinia3Col4, _nrLinia3Col5, _linia1Col1, _linia1Col2, _linia1Col3, _linia1Col4, _linia1Col5,
-               _linia2Col1, _linia2Col2, _linia2Col3, _linia2Col4, _linia2Col5,_linia3Col1, _linia3Col2, _linia3Col3, _linia3Col4,
-               _linia3Col5, _cireasa , _lamaie , _lebenita , _portocale , _pruna , _struguri, _sapte));
+       _spin.addActionListener(new SpinEvent(_slots,_valueSlots, _cireasa , _lamaie , _lebenita , _portocale , _pruna , _struguri, _sapte));
 
        _mainPanel.add(_spin,BorderLayout.PAGE_END);
 
@@ -49,54 +41,56 @@ public class SlotMachine {
         _centerPanel = new JPanel();
         _centerPanel.setLayout(new GridLayout(3,5));
 
-        _linia1Col1 = new SlotStable(_pruna);
-        _nrLinia1Col1 = 5;
-        _linia1Col2 = new SlotStable(_pruna);
-        _nrLinia1Col2 = 5;
-        _linia1Col3 = new SlotStable(_cireasa);
-        _nrLinia1Col3 = 1;
-        _linia1Col4 = new SlotStable(_cireasa);
-        _nrLinia1Col4 = 1;
-        _linia1Col5 = new SlotStable(_lamaie);
-        _nrLinia1Col5 = 2;
-        _linia2Col1 = new SlotStable(_sapte);
-        _nrLinia2Col1 = 7;
-        _linia2Col2 = new SlotStable(_sapte);
-        _nrLinia2Col2 = 7;
-        _linia2Col3 = new SlotStable(_struguri);
-        _nrLinia2Col3 = 6;
-        _linia2Col4 = new SlotStable(_lebenita);
-        _nrLinia2Col4 = 3;
-        _linia2Col5 = new SlotStable(_portocale);
-        _nrLinia2Col5 = 4;
-        _linia3Col1 = new SlotStable(_pruna);
-        _nrLinia3Col1 = 5;
-        _linia3Col2 = new SlotStable(_cireasa);
-        _nrLinia3Col2 = 1;
-        _linia3Col3 = new SlotStable(_lamaie);
-        _nrLinia3Col3 = 2;
-        _linia3Col4 = new SlotStable(_lamaie);
-        _nrLinia3Col4 = 2;
-        _linia3Col5 = new SlotStable(_sapte);
-        _nrLinia3Col5 = 7;
+        _slots = new SlotStable[3][5];
+        _valueSlots = new int[3][5];
 
-        _centerPanel.add(_linia1Col1);
-        _centerPanel.add(_linia1Col2);
-        _centerPanel.add(_linia1Col3);
-        _centerPanel.add(_linia1Col4);
-        _centerPanel.add(_linia1Col5);
+        Random r = new Random();
 
-        _centerPanel.add(_linia2Col1);
-        _centerPanel.add(_linia2Col2);
-        _centerPanel.add(_linia2Col3);
-        _centerPanel.add(_linia2Col4);
-        _centerPanel.add(_linia2Col5);
+        for(int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 5; ++j) {
 
-        _centerPanel.add(_linia3Col1);
-        _centerPanel.add(_linia3Col2);
-        _centerPanel.add(_linia3Col3);
-        _centerPanel.add(_linia3Col4);
-        _centerPanel.add(_linia3Col5);
+                _valueSlots[i][j] = r.nextInt(1, 8);
+
+                switch (_valueSlots[i][j]) {
+                    case 1: {
+                        _slots[i][j] = new SlotStable(_cireasa);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                    case 2: {
+                        _slots[i][j] = new SlotStable(_lamaie);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                    case 3: {
+                        _slots[i][j] = new SlotStable(_lebenita);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                    case 4: {
+                        _slots[i][j] = new SlotStable(_portocale);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                    case 5: {
+                        _slots[i][j] = new SlotStable(_pruna);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                    case 6: {
+                        _slots[i][j] = new SlotStable(_struguri);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                    case 7: {
+                        _slots[i][j] = new SlotStable(_sapte);
+                        _centerPanel.add(_slots[i][j]);
+                        break;
+                    }
+                }
+            }
+
+        }
 
     }
 
